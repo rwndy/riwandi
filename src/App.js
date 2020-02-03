9c74f67c8f4bd9458237705aaa00662f6f1c439c
rwndy <riwandy10@gmail.com>
2020-02-03 23:16:47 +0700

    adding google analytic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Home from './components/Home/index';
 import Footer from './components/Footer/Footer';
+import ReactGA from 'react-ga';
+
+const analytic = () => {
+  const trackingID = 'UA-64780720-1';
+  ReactGA.initialize(trackingID);
+  ReactGA.pageview('/')
+
+}
+
 
 const App = ()=> {
+  analytic()
   return (
     <div className="App">
       <Home />
