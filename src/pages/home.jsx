899933537c8c2db326af985b2996124e0c241f42
rwndy <riwandi@talentkita.com>
2020-05-27 10:59:13 +0700

    build home pages

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const Home = () => {
+  return(
+    <Fragment>
+      <main>
+        <h1>Home</h1>
+      </main>
+    </Fragment>
+  )
+}
+
+export default Home
